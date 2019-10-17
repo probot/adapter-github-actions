a4b71fd5f4b205fff82b7b72a1904e1907d5e65c
Steve Winton <stevewinton@gmail.com>
2019-10-17 10:39:15 -0500

    Add tests

--- index.test.js
+++ index.test.js
@@ -1,5 +1,46 @@
+jest.mock('uuid');
+jest.mock('@actions/core');
+jest.mock('probot');
+
+const uuid = require('uuid');
+
+const { createProbot } = require('probot');
+
+const adapt = require('./index');
+
 describe('probot-actions-adapter', () => {
-  test('that we have tests', () => {
-    expect(1 + 1).toBe(2);
+  let probot;
+
+  beforeEach(() => {
+    // Mock uuid
+    uuid.v4 = jest.fn(() => 'uuid-v4');
+
+    // Mock probot
+    probot = {
+      setup: jest.fn(),
+      receive: jest.fn(async () => true)
+    };
+
+    createProbot.mockImplementation(() => {
+      return probot;
+    });
+  });
+
+  test('that we can adapt a single handler', async () => {
+    const handler = () => {};
+    await adapt(handler);
+
+    expect(createProbot).toHaveBeenCalledWith({ githubToken: 'GITHUB_TOKEN' });
+    expect(probot.setup).toHaveBeenCalledWith([handler]);
+    expect(probot.receive).toHaveBeenCalledWith({ id: 'uuid-v4', name: 'push', payload: { commits: [] } });
+  });
+
+  test('that we can adapt many handlers', async () => {
+    const handlers = [() => {}, () => {}];
+    await adapt(...handlers);
+
+    expect(createProbot).toHaveBeenCalledWith({ githubToken: 'GITHUB_TOKEN' });
+    expect(probot.setup).toHaveBeenCalledWith(handlers);
+    expect(probot.receive).toHaveBeenCalledWith({ id: 'uuid-v4', name: 'push', payload: { commits: [] } });
   });
 });
---
+++ jest.setup-env.js
@@ -0,0 +1,3 @@
+process.env.GITHUB_TOKEN = 'GITHUB_TOKEN';
+process.env.GITHUB_EVENT_NAME = 'push';
+process.env.GITHUB_EVENT_PATH = './fixtures/push.json';
