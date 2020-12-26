4ade1d3246c72e4b000b364bd424ced4219be64b
Joseph Petersen <josephp90@gmail.com>
2020-12-26 21:44:07 +0100

    adapt to probot create

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 process.env.DISABLE_WEBHOOK_EVENT_CHECK = 'true';
 
 const path = require('path');
-const uuid = require('uuid');
 
 const core = require('@actions/core');
 
-const { Probot } = require('probot');
+const { createProbot: probotCreate } = require('probot');
 
-module.exports = (...handlers) => {
-  // Setup Probot app
-  const githubToken = process.env.GITHUB_TOKEN;
-  const probot = new Probot({ githubToken });
-  probot.setup(handlers);
+function createProbot({
+  overrides = {},
+  defaults = {},
+  env = process.env,
+}) {
+  overrides.githubToken = process.env.GITHUB_TOKEN;
+  return probotCreate({ overrides, defaults, env })
+}
+
+async function runProbot (...handlers, { probot = createProbot }) {
+  await probot.load(handlers);
 
   // Process the event
   const event = process.env.GITHUB_EVENT_NAME;
@@ -19,9 +24,17 @@ module.exports = (...handlers) => {
   // eslint-disable-next-line global-require, import/no-dynamic-require
   const payload = require(path.resolve(payloadPath));
   core.debug(`Receiving event ${JSON.stringify(event)}`);
-  return probot.receive({ name: event, payload, id: uuid.v4() }).catch(err => {
+  return probot.receive({
+    id: process.env.GITHUB_RUN_ID,
+    name: event,
+    payload
+  }).catch(err => {
     // setFailed logs the message and sets a failing exit code
     core.setFailed(`Action failed with error: ${err.message}`);
     throw err;
   });
-};
+}
+
+exports.runProbot = runProbot
+
+exports.createProbot = createProbot
