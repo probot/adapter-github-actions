436f064d98c560a232b62489e4c4d3074f6f1190
Aras Abbasi <aras.abbasi@googlemail.com>
2025-08-07 02:58:56 +0200

    also destructure name

--- pino-transport-github-actions.js
+++ pino-transport-github-actions.js
@@ -14,7 +14,7 @@ const LEVEL_TO_ACTIONS_CORE_LOG_METHOD = {
 };
 
 export const transport = through.obj(function (chunk, enc, cb) {
-  const { level, hostname, pid, msg, time, ...meta } = JSON.parse(chunk);
+  const { level, hostname, pid, msg, time, name, ...meta } = JSON.parse(chunk);
   const levelLabel = pino.levels.labels[level] || level;
   const logMethodName = LEVEL_TO_ACTIONS_CORE_LOG_METHOD[levelLabel];
 
